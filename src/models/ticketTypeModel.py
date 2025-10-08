48948e0721077b32f113c42d56ca664a12ac8c1b
StealKkak <155833646+StealKkak@users.noreply.github.com>
2025-10-08 12:48:15 +0900

    feat: 티켓 설정에 설명 추가

--- src/models/ticketTypeModel.py
+++ src/models/ticketTypeModel.py
@@ -3,9 +3,10 @@ from services.dbService import *
 from utils.arrayUtil import *
 
 class TicketType:
-    def __init__(self, guild, name: str, userClose: bool, dupTicket: bool, role: list, survey1: str = None, survey2: str = None, survey3: str = None, ticketCategory: str=None, closedTicketCategory: str=None, id=None):
+    def __init__(self, guild, name: str, description: str, userClose: bool, dupTicket: bool, role: list, survey1: str = None, survey2: str = None, survey3: str = None, ticketCategory: str=None, closedTicketCategory: str=None, id=None):
         self.__guild = guild
         self.__name = name
+        self.__description = description
         self.__survey1 = survey1
         self.__survey2 = survey2
         self.__survey3 = survey3
@@ -31,6 +32,14 @@ class TicketType:
     @name.setter
     def name(self, value):
         self.__name = value
+
+    @property
+    def description(self):
+        return self.__description
+    
+    @description.setter
+    def description(self, value):
+        self.__description = value
 
     @property
     def survey1(self):
@@ -109,7 +118,7 @@ class TicketType:
             await closeDB(con, cur)
             raise ValueError("A record with the same guild and name already exists.")
         
-        await cur.execute("UPDATE ticket_settings SET name = ?, survey1 = ?, survey2 = ?, survey3 = ?, role = ?, user_close = ?, dup_ticket = ?, ticket_category = ?, closed_ticket_category = ? WHERE id = ?", (self.__name, self.__survey1, self.__survey2, self.__survey3, self.__role, self.__userClose, self.__dupTicket, self.__ticketCategory, self.__closedTicketCategory, self.__id,))
+        await cur.execute("UPDATE ticket_settings SET name = ?, description = ?, survey1 = ?, survey2 = ?, survey3 = ?, role = ?, user_close = ?, dup_ticket = ?, ticket_category = ?, closed_ticket_category = ? WHERE id = ?", (self.__name, self.__description, self.__survey1, self.__survey2, self.__survey3, self.__role, self.__userClose, self.__dupTicket, self.__ticketCategory, self.__closedTicketCategory, self.__id,))
         await con.commit()
         await closeDB(con, cur)
 
@@ -120,7 +129,7 @@ class TicketType:
         await closeDB(con, cur)
 
     @staticmethod
-    async def createInstance(guild, name: str, userClose: bool, dupTicket: bool, role: list, survey1: str = None, survey2: str = None, survey3: str = None, ticketCategory:str = None, closedTicketCategory:str = None):
+    async def createInstance(guild, name: str, description: str, userClose: bool, dupTicket: bool, role: list, survey1: str = None, survey2: str = None, survey3: str = None, ticketCategory:str = None, closedTicketCategory:str = None):
         con, cur = await loadDB()
         await cur.execute("SELECT * FROM ticket_settings WHERE guild = ? AND name = ?", (guild, name))
         exists = await cur.fetchone()
@@ -128,11 +137,11 @@ class TicketType:
             await closeDB(con, cur)
             raise ValueError("A record with the same guild and name already exists.")
         
-        await cur.execute("INSERT INTO ticket_settings (guild, name, survey1, survey2, survey3, role, user_close, dup_ticket, ticket_category, closed_ticket_category) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (guild, name, survey1, survey2, survey3, arrayToString(role), 1 if userClose else 0, 1 if dupTicket else 0, ticketCategory, closedTicketCategory))
+        await cur.execute("INSERT INTO ticket_settings (guild, name, description, survey1, survey2, survey3, role, user_close, dup_ticket, ticket_category, closed_ticket_category) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (guild, name, description, survey1, survey2, survey3, arrayToString(role), 1 if userClose else 0, 1 if dupTicket else 0, ticketCategory, closedTicketCategory))
         await con.commit()
         id = cur.lastrowid
         await closeDB(con, cur)
-        return TicketType(guild, name, userClose, dupTicket, role, survey1, survey2, survey3, ticketCategory, closedTicketCategory, id)
+        return TicketType(guild, name, description, userClose, dupTicket, role, survey1, survey2, survey3, ticketCategory, closedTicketCategory, id)
 
     @staticmethod
     async def findByGuildIdAndName(guildId, name) -> "TicketType":
@@ -144,7 +153,7 @@ class TicketType:
         if not row:
             return None
         
-        return TicketType(row["guild"], row["name"], bool(row["user_close"]), row["dup_ticket"], stringToArray(row["role"]), row["survey1"], row["survey2"], row["survey3"], row["ticket_category"], row["closed_ticket_category"], row["id"])
+        return TicketType(row["guild"], row["name"], row["description"], bool(row["user_close"]), row["dup_ticket"], stringToArray(row["role"]), row["survey1"], row["survey2"], row["survey3"], row["ticket_category"], row["closed_ticket_category"], row["id"])
     
     @staticmethod
     async def findByGuildId(guildId) -> list["TicketType"]:
@@ -156,7 +165,7 @@ class TicketType:
         await closeDB(con, cur)
         
         for row in rows:
-            result.append(TicketType(row["guild"], row["name"], bool(row["user_close"]), row["dup_ticket"], stringToArray(row["role"]), row["survey1"], row["survey2"], row["survey3"], row["ticket_category"], row["closed_ticket_category"], row["id"]))
+            result.append(TicketType(row["guild"], row["name"], row["description"], bool(row["user_close"]), row["dup_ticket"], stringToArray(row["role"]), row["survey1"], row["survey2"], row["survey3"], row["ticket_category"], row["closed_ticket_category"], row["id"]))
         
         return result
     
@@ -167,4 +176,4 @@ class TicketType:
         row = await cur.fetchone()
         await closeDB(con, cur)
 
-        return TicketType(row["guild"], row["name"], bool(row["user_close"]), row["dup_ticket"], stringToArray(row["role"]), row["survey1"], row["survey2"], row["survey3"], row["ticket_category"], row["closed_ticket_category"], row["id"]) if row else None
+        return TicketType(row["guild"], row["name"], row["description"], bool(row["user_close"]), row["dup_ticket"], stringToArray(row["role"]), row["survey1"], row["survey2"], row["survey3"], row["ticket_category"], row["closed_ticket_category"], row["id"]) if row else None
